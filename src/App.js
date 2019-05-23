aa239bdc8adb6dc4696f78abc8dc17f5da6fe32c
Kamil <developer.kamil90@gmail.com>
2019-05-23 18:41:17 +0200

    api connected

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import { Provider } from 'react-redux';
 import store from './store'
 import "bootstrap/dist/css/bootstrap.min.css";
+import SearchResults from './components/SearchResults/SearchResults';
 
 function App() {
   return (
     <Provider store={store}>
-      <div>
-        <h1>Hello</h1>
+      <div className="container">
+        <SearchResults />
       </div>
     </Provider>
 
---
+++ src/actions/meteoriteActions.js
@@ -0,0 +1,41 @@
+import {
+    FETCH_METEORITES_BEGIN,
+    FETCH_METEORITES_SUCCESS,
+    FETCH_METEORITES_FAILURE
+} from './types'
+
+export const fetchMeteoritesRequest = () => ({
+    type: FETCH_METEORITES_BEGIN
+})
+
+export const fetchMeteoritesSuccess = (meteorites) => ({
+    type: FETCH_METEORITES_SUCCESS,
+    payload: meteorites
+})
+
+export const fetchMeteoritesFailure = (error) => ({
+    type: FETCH_METEORITES_FAILURE,
+    payload: error
+})
+
+export const fetchMeteorites = (searchTerm) => (dispatch) => {
+    dispatch(fetchMeteoritesRequest())
+    let url;
+    if (searchTerm) {
+        url = `https://data.nasa.gov/resource/gh4g-9sfh.json?$where=lower(name)%20like%20lower(%22%25${searchTerm}%25%22)`;
+    } else {
+        url = `https://data.nasa.gov/resource/gh4g-9sfh.json?$limit=50000&$offset=0`;
+    }
+    return fetch(url)
+        .then(handleErrors)
+        .then(res => res.json())
+        .then(data => dispatch(fetchMeteoritesSuccess(data)))
+        .catch(error => dispatch(fetchMeteoritesFailure(error)))
+}
+
+const handleErrors = (response) => {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
